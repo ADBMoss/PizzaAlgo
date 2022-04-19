493d63a269eab82bfb2d1c012242fd4520711018
DarriusB <darrius.bradford@gmail.com>
2022-04-19 12:21:06 -0700

    Added additional instructions for  if pizza is cold

--- Algo/Program.cs
+++ Algo/Program.cs
@@ -1,6 +1,8 @@
-﻿/*Decide where to order pizza from
+﻿*Decide where to order pizza from
 Decide what type of pizza I want
 Call  Pizza Business and place order
 wait for pizza to arrive
 Pay dilivery driver
-Take delievery of pizza*/
+Take delievery of pizza
+
+    warm pizza if cold.
